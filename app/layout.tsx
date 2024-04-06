2942806ec898faeaadaa3ce544c6a83fdd151e30
leesiyuan <lee.si.yuan.20@gmail.com>
2024-04-06 21:19:57 +0800

    Remove footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from '@/components/ui/sonner'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
+    default: 'Govern Analysis Chatbot',
     template: `%s - Next.js AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
