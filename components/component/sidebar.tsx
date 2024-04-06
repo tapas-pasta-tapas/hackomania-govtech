85c3f04f0cb0e6545975386db2ac6b0b98dfacbf
leezhengjing <zhengjing.lee86@gmail.com>
2024-04-06 22:40:21 +0800

    Complete Fix of Misalignment

--- components/component/sidebar.tsx
+++ components/component/sidebar.tsx
@@ -10,7 +10,7 @@ export function SidebarNew() {
                 <div className="flex h-[60px] items-center border-b px-6">
                     <Link className="flex items-center gap-2 font-semibold" href="#">
                         <Package2Icon className="size-6" />
-                        <span className="">Acme Inc</span>
+                        <span className="">Govern Analysis</span>
                     </Link>
                     {/* <Button className="ml-auto size-8" size="icon" variant="outline">
                         <BellIcon className="size-4" />
@@ -20,7 +20,7 @@ export function SidebarNew() {
                 <div className="flex-1 overflow-auto py-2">
                     <nav className="grid items-start px-4 text-sm font-medium">
                         <Link
-                            className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-zinc-950 dark:text-gray-50 dark:hover:text-gray-50"
+                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                             href="/dashboard"
                         >
                             <HomeIcon className="size-4" />
